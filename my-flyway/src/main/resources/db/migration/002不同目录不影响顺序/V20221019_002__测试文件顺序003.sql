0cf3bbe6532247cb51a7a3f39df30a0a67c1792f
hongjunjie <hongjunjie@syncsoft.com>
2023-05-11 11:47:18 +0800

    删去 flyway

--- my-flyway/src/main/resources/db/migration/002不同目录不影响顺序/V20221019_002__测试文件顺序003.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO user (id, name, age, email) VALUES
-(100, '张三', 48, 'test100@baomidou.com'),
-(200, '李四', 50, 'test200@baomidou.com'),
-(300, '王五', 48, 'test300@baomidou.com'),
-(400, '赵六', 51, 'test400@baomidou.com');
-
-
--- 如果本文件优先于 002 文件，则数据都会不会是99
-update user set age = 99 where id = '5';
